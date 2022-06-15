3c651008d21470a59f7f8dd665c2f0883d699c7c
margaret manyeki <maggiemanyeki6@gmail.com>
2022-06-15 15:03:19 +0300

    commit tree a06d346cacae37a987b0d7af09278c9695d51d6f

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ const init = () => {
         event.preventDefault();
         const input = document.querySelector('input#searchByID');
         console.log(input.value);
-        fetch('http://localhost:3000/movies')
+        fetch(`http://localhost:3000/movies/${input.value}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+
         });
     });
 }
